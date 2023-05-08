d51b1dbe5542fe0388fdeacae33d88de0f8a8b74
halilillo <halilillo@yandex.ru>
2023-05-08 13:25:29 +0500

    2

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import Promise, render, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, render#, get_object_or_404 ,Promise,
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .models import Post
+from .forms import EmailPostForm
 
 
 def post_list(request):
@@ -26,8 +27,23 @@ def post_detail(request, year, month, day, post):
                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
 
+
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_share(request, post_id):  
+    post = get_object_or_404(Post,
+                            id=post_id,
+                            status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form})
